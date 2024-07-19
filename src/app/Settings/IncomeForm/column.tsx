9ffdba7b93027d398138cb276f952cfa14167351
Flawyer <Nimadongtamang1@gmail.com>
2024-07-19 09:43:02 +0545

    Some Settings Changed

--- src/app/Settings/IncomeForm/column.tsx
+++ src/app/Settings/IncomeForm/column.tsx
@@ -5,7 +5,7 @@ import { ColumnDef } from "@tanstack/react-table"
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type IncomeForm = {
-  id: string
+  _id: string
   name: string
   description: string
   sn?: number;
@@ -19,9 +19,5 @@ export const columns: ColumnDef<IncomeForm>[] = [
   {
     accessorKey: "name",
     header: "Name",
-  },
-  {
-    accessorKey: "description",
-    header: "Description",
   }
 ]
